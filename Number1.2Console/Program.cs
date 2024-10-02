178411c62fd1d3b538887c30ac1e6ee4574bfd63
GrgijKD <svidpustka@gmail.com>
2024-10-02 11:51:31 +0300

    first commit

---
+++ Number1.2Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        int number;
+        bool isValid;
+        
+        do
+        {
+            Console.WriteLine("Введіть ціле число:");
+            string input = Console.ReadLine();
+
+            isValid = int.TryParse(input, out number);
+
+            if (!isValid)
+            {
+                Console.WriteLine("Неправильне число. Спробуйте ще раз.");
+            }
+
+        } while (!isValid);
+
+        Console.WriteLine($"Ви ввели число {number}");
+    }
+}
